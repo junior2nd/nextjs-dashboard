0516380d0853ca1a46766ce90282a041b83f6495
supanut meesuk <juniorsut@gmail.com>
2023-11-27 16:32:45 +0700

    Update Table invoices

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -87,9 +87,11 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
+                        alt={'test'}
                         className="rounded-full"
                         width={28}
-                        height={28} alt={''}                      />
+                        height={28}
+                      />
                       <p>{invoice.name}</p>
                     </div>
                   </td>
